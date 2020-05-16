4891754756d223d092539eaf1002ff4fa9cd86ce
iamSebastianTeo <TE0008AN@e.ntu.edu.sg>
2020-05-16 12:25:09 +0800

    Added removing preappend zeros to price into preprocessing function

--- Data_Pipeline.py
+++ Data_Pipeline.py
@@ -21,30 +21,40 @@ print(df.info())
 
 def processing_fn(x):
 
-	try:
-		#remove leading and trailing whitespace of the name 
-		name_list = x['name'].strip()
-		#split by the space inbetween the first_name and the last_name
-		name_list = name_list.split(' ')
+    try:
+        #remove leading and trailing whitespace of the name 
+        name_list = x['name'].strip()
+        #split by the space inbetween the first_name and the last_name
+        name_list = name_list.split(' ')
 
-		#This will take cares of people who did not put their last name.
-		if len(name_list) == 1:
-			first_name = name_list[0]
-			last_name = None
+        #This will take cares of people who did not put their last name.
+        if len(name_list) == 1:
+            first_name = name_list[0]
+            last_name = None
 
-		#This will take cares of people who put thier middle name. 
-		elif len(name_list) >= 2:
-			first_name = name_list[0]
-			last_name = name_list[-1]
-            
-		x['first_name'] = first_name
-		x['last_name'] = last_name
+        #This will take cares of people who put thier middle name. 
+        elif len(name_list) >= 2:
+            first_name = name_list[0]
+            last_name = name_list[-1]
 
-	except Exception as e:
-		print('error occured in processing_fn',e)
+        #Change the float type of the price to string 
+        price = str(x['price'])
+        #Remove any zero preappend to the price field 
+        price = price.strip('0')
 
-	else:
-		return(x)
+        #Change the string type of price back to float. 
+        price = float(price)
+        print(price)
+
+        x['first_name'] = first_name
+        x['last_name'] = last_name
+        x['processed_price'] = price
+
+    except Exception as e:
+        print('error occured in processing_fn',e)
+
+    else:
+        return(x)
 
 df = df.apply(processing_fn,axis=1)
 print(df.head())
